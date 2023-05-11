4a10478d5ee03cb923c7b6b807976502e00c605c
VictorNunesVN <58942853+VictorNunesVN@users.noreply.github.com>
2023-05-11 17:52:27 -0300

    modificação e criação
    Modificando arquivos e criando um arquivo para a função que fará a validação do que foi dito.

--- js/reconhecimentoDeVoz.js
+++ js/reconhecimentoDeVoz.js
@@ -1,1 +1,28 @@
+const elementoChute = document.getElementById('chute')
+// Veio do site da mozila 
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+// Veio do site da mozila 
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
+const recognition = new SpeechRecognition();
+//Configurando o RECOGNITION para o português do Brasil.
+recognition.lang = 'pt-br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(evento){
+   chute = evento.results[0][0].transcript
+   console.log(evento) // mostra a class SpeechRecognition() com seus atributos na tela
+   exibeChuteNaTela(chute)
+   verificaSeOChutePossuiUmValorValido(chute)
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class= 'box'>${chute}</span>
+    `
+}
+
